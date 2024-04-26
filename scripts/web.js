052979f1033b74e6db87eea19b90319ef9bdd6ff
psalzano <paolo.salzano@outlook.it>
2024-04-26 23:46:41 +0200

    Update 1.0.0: The project is Ping Pong-ify (not definitive name)

---
+++ scripts/web.js
@@ -0,0 +1,14 @@
+var menuItems = document.querySelectorAll('li.has-submenu');
+Array.prototype.forEach.call(menuItems, function(el, i){
+	el.querySelector('a').addEventListener("click",  function(event){
+		if (this.parentNode.className == "has-submenu") {
+			this.parentNode.className = "has-submenu open";
+			this.setAttribute('aria-expanded', "true");
+		} else {
+			this.parentNode.className = "has-submenu";
+			this.setAttribute('aria-expanded', "false");
+		}
+		event.preventDefault();
+		return false;
+	});
+});
